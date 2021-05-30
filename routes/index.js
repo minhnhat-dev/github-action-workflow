65877397e22d59a07b459b6de7b1e4a92e64e953
Nguyen Nhat <nhat.nguyen@kingfood.co>
2021-05-30 12:47:39 +0700

    init app

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/ping', (req, res, next) => {
+    res.status(200).send({ message: 'pong' });
+});
+
+module.exports = router;
